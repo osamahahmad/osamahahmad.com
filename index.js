3c64017fd689491acdcfe111bdef39b28b029446
Osamah Ahmad <me@osamahahmad.com>
2023-01-01 08:52:43 +0000

    removed connect and serve-static in lieu of express

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var connect = require('connect')
-var serveStatic = require('serve-static')
+var express = require('express')
 var vhost = require('vhost')
 
-var app = connect()
+var app = express()
 
-var osamahahmad_com = connect()
-osamahahmad_com.use(serveStatic(__dirname + '/osamahahmad.com'))
+var osamahahmad_com = express()
+osamahahmad_com.use(express.static(__dirname + '/osamahahmad.com'))
 app.use(vhost('osamahahmad.com', osamahahmad_com))
 
-app.use(serveStatic(__dirname + '/static'))
+app.use(express.static(__dirname + '/static'))
 
 app.listen(80)
